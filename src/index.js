520a99f7282dcee119c3097061314d0b5c1d5a42
Var4nam <Varunkumarbarra@gmail.com>
2024-05-08 14:55:28 +0530

    [Varun][08.05.24][change hight and width]

--- src/index.js
+++ src/index.js
@@ -1,28 +1,27 @@
-
 import Phaser from "phaser";
 
 const config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  width: 1000,
+  height: 900,
   physics: {
-    default: 'arcade',
+    default: "arcade",
     arcade: {
-      gravity: { y: 200 }
-    }
+      gravity: { y: 200 },
+    },
   },
   scene: {
     preload: preload,
-    create: create
-  }
+    create: create,
+  },
 };
 
 new Phaser.Game(config);
 
-function preload () {
-  this.load.image('sky', 'assets/sky.png');
+function preload() {
+  this.load.image("sky", "assets/sky.png");
 }
 
-function create () {
-  this.add.image(400, 300, 'sky');
+function create() {
+  this.add.image(500, 450, "sky");
 }
