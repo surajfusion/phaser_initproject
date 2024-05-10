bfd284e53bc5390065485550007fb9bd73454742
Suraj <suraj@aifly.io>
2024-05-10 22:01:11 +0530

    SpriteandItsUser

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const gameSpeedofY = 300;
 
 //game variable;
 let birdImage;
+let startsGroup;
 
 const config = {
   type: Phaser.AUTO,
@@ -26,26 +27,57 @@ new Phaser.Game(config);
 function preload () {
   this.load.image('sky', 'assets/sky.png');
   this.load.image('bird', 'assets/bird.png');
-  this.load.image('star', 'assets/star.png');
+  for(let i =0; i < 5; i++){
+    this.load.image('star_' + i, 'assets/star.png');
+  }
+  
   this.load.image('bomb', 'assets/bomb.png');
 }
 
 function create () {
   this.add.image(0, 0, 'sky').setOrigin(0);
-  this.add.image(700, 0, 'star');
+
+  for(let i = 0; i < 5; i++){
+    startsGroup = this.physics.add.image(getRandomInt(750), getRandomInt(300), `star_${i}`)
+      .setOrigin(0).setVelocityX(-30);
+  }
+  
+  
   //this.add.image(60, 300, 'bird');
   //const logo = this.physics.add.image(400, 100, 'bomb');
 
-  birdImage = this.physics.add.image(60, 300, 'bird').setOrigin(1);
-  birdImage.setVelocityY(100).setVelocityX(100);
+  //birdImage = this.physics.add.image(60, 300, 'bird').setOrigin(1);
+  birdImage = this.physics.add.sprite(60, 300, 'bird')
+                .setOrigin(1)
+                .setInteractive();
+  //birdImage.setVelocityY(100).setVelocityX(100);
+
+  var spaceBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+  spaceBar.on('down', () =>{
+    console.log('SPACE DOWN');
+    birdImage.setVelocityY(-200);
+  });
+
+  spaceBar.on('up', () =>{
+    console.log('SPACE UP');
+  });
 }
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
 
 function update() {
   //console.log('UPDATE')
-  console.log(birdImage.body.position.x, birdImage.body.position.y)
+  //console.log(birdImage.body.position.x, birdImage.body.position.y)
   if(birdImage.body.position.y > 600){
     //birdImage.body.position.y = 300;
     birdImage.setVelocityY(-100)
   }
+
+  console.log(startsGroup.body.position.x);
+  console.log(startsGroup.body.position.x);
+  //debugger;
+  
+  
 }
