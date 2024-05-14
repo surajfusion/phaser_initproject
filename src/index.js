a26f0db5deb95d55d823a38e1b8604cfe54eebf0
Suraj <suraj@aifly.io>
2024-05-14 21:49:30 +0530

    Grouping+InfiniteScreen

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import Phaser from "phaser";
 
 const gameSpeedofX = 300;
 const gameSpeedofY = 300;
+const MAX_STAR_POS = 1500;
+const MAX_START_HEIGHT = 300;
+const FIXED_GAP = 200;
+let LAST_POS = 0;
+let NUMBER_OF_STAR = 20;
 
 //game variable;
 let birdImage;
@@ -28,7 +33,7 @@ new Phaser.Game(config);
 function preload () {
   this.load.image('sky', 'assets/sky.png');
   this.load.image('bird', 'assets/bird.png');
-  for(let i =0; i < 5; i++){
+  for(let i =0; i < NUMBER_OF_STAR; i++){
     this.load.image('star_' + i, 'assets/star.png');
   }
   
@@ -37,46 +42,32 @@ function preload () {
 
 function create () {
   this.add.image(0, 0, 'sky').setOrigin(0);
-  startsGroup = this.physics.add.group();
-  startsGroup2 = this.physics.add.group();
 
-  for(let i = 0; i < 5; i++){
-    startsGroup.create(getRandomInt(700), getRandomInt(300), `star_${i}`);
-  }
-
-  for(let i = 0; i < 5; i++){
-    startsGroup2.create(getRandomInt(700), getRandomInt(300), `star_${i}`);
-  }
-
-  startsGroup2.scaleXY(0.5,0.5);
-
-  startsGroup.setVelocityX(-60);
-
-  startsGroup.getChildren().forEach((star)=>{
-    console.log(star.x);
-
-  });
-
+  createStars(this);
   console.log(startsGroup);
-  
   
   birdImage = this.physics.add.sprite(60, 300, 'bird')
                 .setOrigin(1)
                 .setInteractive();
 
-  var spaceBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-  spaceBar.on('down', () =>{
-    console.log('SPACE DOWN');
-    birdImage.setVelocityY(-200);
-    birdImage.setGravityY(200);
+  handleUserInput(this);
+}
 
-    startsGroup.scaleXY(0.3,0.3);
-    startsGroup2.setVelocityY(150);
-  });
+function createStars(context){
+  startsGroup = context.physics.add.group();
+  for(let i = 0; i < NUMBER_OF_STAR; i++){
+    startsGroup.create(getRandomInt(MAX_STAR_POS) + LAST_POS + FIXED_GAP, getRandomInt(config.height), `star_${i}`)
+    .setVelocityX(-getRandomInt(90));
+  }
+  //startsGroup.setVelocityX(-90);
+}
 
-  spaceBar.on('up', () =>{
-    console.log('SPACE UP');
+function getLastStarPosition(){
+  let lastStartPos = 0;
+  startsGroup.getChildren().forEach((star)=>{
+    lastStartPos = Math.max(star.body.x, lastStartPos);
   });
+  return lastStartPos;
 }
 
 function getRandomInt(max) {
@@ -90,6 +81,27 @@ function update() {
     //birdImage.body.position.y = 300;
     birdImage.setVelocityY(-100)
   }
-  
-  
+
+  if(getLastStarPosition() < config.width){
+    LAST_POS = getLastStarPosition();
+    createStars(this);
+  }
+  //console.log(getLastStarPosition());
+}
+
+
+function handleUserInput(context){
+  var upArrow = context.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+  upArrow.on('down', () => {
+    console.log('SPACE DOWN');
+    birdImage.setVelocityY(-200);
+    birdImage.setVelocityX(0);
+  });
+
+  var downArrow = context.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+  downArrow.on('down', () => {
+    console.log('SPACE DOWN');
+    birdImage.setVelocityY(200);
+    birdImage.setVelocityX(0);
+  });
 }
