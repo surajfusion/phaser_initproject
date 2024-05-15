31ddaf3f46afab068870c76b2b88627b1ec6ca2c
manish56g <manish@aifly.io>
2024-05-15 17:30:11 +0530

    [m]

--- src/index.js
+++ src/index.js
@@ -1,28 +1,130 @@
 
 import Phaser from "phaser";
 
+const gameSpeedofX = 300;
+const gameSpeedofY = 300;
+
+//game variable;
+let birdImage;
+let startsGroup;
+let startsGroup2;
+
 const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
   physics: {
     default: 'arcade',
-    arcade: {
-      gravity: { y: 200 }
-    }
   },
   scene: {
     preload: preload,
-    create: create
+    create: create,
+    update: update
   }
 };
 
 new Phaser.Game(config);
 
-function preload () {
+function preload() {
   this.load.image('sky', 'assets/sky.png');
+  this.load.image('bird', 'assets/bird.png');
+  for (let i = 0; i < 5; i++) {
+    this.load.image('star_' + i, 'assets/star.png');
+  }
+
+  this.load.image('bomb', 'assets/bomb.png');
+}
+let cursors;
+
+function create() {
+  cursors = this.input.keyboard.createCursorKeys()
+  this.add.image(0, 0, 'sky').setOrigin(0);
+  startsGroup = this.physics.add.group();
+  startsGroup2 = this.physics.add.group();
+
+  for (let i = 0; i < 5; i++) {
+    startsGroup.create(getRandomInt(700), getRandomInt(300), `star_${i}`);
+  }
+
+  for (let i = 0; i < 5; i++) {
+    startsGroup2.create(getRandomInt(700), getRandomInt(300), `star_${i}`);
+  }
+
+  startsGroup2.scaleXY(0.5, 0.5);
+
+  startsGroup.setVelocityX(-60);
+
+  startsGroup.getChildren().forEach((star) => {
+    console.log(star.x);
+
+  });
+
+  console.log(startsGroup);
+
+
+  birdImage = this.physics.add.sprite(60, 300, 'bird')
+    .setOrigin(1)
+    .setInteractive();
+
+
+  var right = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+  right.on('right', () => {
+    console.log('right DOWN');
+
+    birdImage.setVelocityY(100);
+
+  });
+
+  var spaceBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+  spaceBar.on('down', () => {
+    console.log('SPACE DOWN');
+    // birdImage.setVelocityY(-200);
+    // birdImage.setGravityY(200);
+    //birdImage.setVelocityY(100);
+
+    startsGroup.scaleXY(0.3, 0.3);
+    startsGroup2.setVelocityY(150);
+  });
+
+  spaceBar.on('up', () => {
+    console.log('SPACE UP');
+  });
 }
 
-function create () {
-  this.add.image(400, 300, 'sky');
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function update() {
+  //console.log('UPDATE')
+  //console.log(birdImage.body.position.x, birdImage.body.position.y)
+  if (birdImage.body.position.y > 600) {
+    //birdImage.body.position.y = 300;
+    birdImage.setVelocityY(-100)
+  }
+
+
+  if (cursors.right.isDown) {
+    console.log('SPACE UP   dsd');
+    birdImage.setVelocityX(100);
+    birdImage.setVelocityY(0);
+    //text.x += 5
+  }
+  if (cursors.left.isDown) {
+    birdImage.setVelocityX(-100);
+    birdImage.setVelocityY(0);
+   // text.x -= 5
+  }
+  if (cursors.up.isDown) {
+    birdImage.setVelocityY(-100);
+    birdImage.setVelocityX(0);
+   // text.y -= 5
+  }
+  if (cursors.down.isDown) {
+    birdImage.setVelocityY(100);
+    birdImage.setVelocityX(0);
+    //text.y -= 5
+  }
+
+
 }
