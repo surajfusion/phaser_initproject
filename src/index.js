672a3fcbfdee1d467d5cd8c36c71322e3109a025
manish56g <manish@aifly.io>
2024-05-16 11:04:25 +0530

    [manish][collider]

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import Phaser from "phaser";
 
 const gameSpeedofX = 300;
 const gameSpeedofY = 300;
+const MAX_STAR_POS = 1500;
+const MAX_START_HEIGHT = 300;
+let FIXED_GAP = 80;
+let LAST_POS = 0;
+let NUMBER_OF_STAR = 8;
 
 //game variable;
 let birdImage;
@@ -15,80 +20,73 @@ const config = {
   height: 600,
   physics: {
     default: 'arcade',
+    arcade: {
+      debug: true
+    }
   },
   scene: {
     preload: preload,
     create: create,
-    update: update
+    update:update
   }
 };
 
 new Phaser.Game(config);
 
-function preload() {
+function preload () {
   this.load.image('sky', 'assets/sky.png');
   this.load.image('bird', 'assets/bird.png');
-  for (let i = 0; i < 5; i++) {
+  this.load.image('bomb', 'assets/bomb.png');
+  for(let i =0; i < NUMBER_OF_STAR; i++){
     this.load.image('star_' + i, 'assets/star.png');
   }
-
+  
   this.load.image('bomb', 'assets/bomb.png');
 }
-let cursors;
 
-function create() {
-  cursors = this.input.keyboard.createCursorKeys()
+function create () {
   this.add.image(0, 0, 'sky').setOrigin(0);
-  startsGroup = this.physics.add.group();
-  startsGroup2 = this.physics.add.group();
 
-  for (let i = 0; i < 5; i++) {
-    startsGroup.create(getRandomInt(700), getRandomInt(300), `star_${i}`);
-  }
-
-  for (let i = 0; i < 5; i++) {
-    startsGroup2.create(getRandomInt(700), getRandomInt(300), `star_${i}`);
-  }
-
-  startsGroup2.scaleXY(0.5, 0.5);
-
-  startsGroup.setVelocityX(-60);
-
-  startsGroup.getChildren().forEach((star) => {
-    console.log(star.x);
-
-  });
-
+  createStars(this);
   console.log(startsGroup);
+  
+  birdImage = this.physics.add.sprite(90, 300, 'bird')
+                .setOrigin(0)
+                .setInteractive();
 
+  handleUserInput(this);
 
-  birdImage = this.physics.add.sprite(60, 300, 'bird')
-    .setOrigin(1)
-    .setInteractive();
+  this.physics.add.collider(birdImage, startsGroup, collideCallback);
 
+  //4. Add Collider between bomb and Star.
+  //After collision, destroy Star.
+}
 
-  var right = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
-  right.on('right', () => {
-    console.log('right DOWN');
+function createStars(context){
+  startsGroup = context.physics.add.group();
+  /*
+  for(let i = 0; i < NUMBER_OF_STAR; i++){
+    startsGroup.create(getRandomInt(MAX_STAR_POS) + LAST_POS + FIXED_GAP, getRandomInt(config.height), `star_${i}`)
+    .setVelocityX(-getRandomInt(90));
+  }*/
 
-    birdImage.setVelocityY(100);
+  for(let i = 0; i < NUMBER_OF_STAR; i++){
+    startsGroup.create(750, i* FIXED_GAP, `star_${i}`).setOrigin(0);
+  }
+  
+  //startsGroup.setVelocityX(-90);
+}
 
+function fire(){
+
+}
+
+function getLastStarPosition(){
+  let lastStartPos = 0;
+  startsGroup.getChildren().forEach((star)=>{
+    lastStartPos = Math.max(star.body.x, lastStartPos);
   });
-
-  var spaceBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-  spaceBar.on('down', () => {
-    console.log('SPACE DOWN');
-    // birdImage.setVelocityY(-200);
-    // birdImage.setGravityY(200);
-    //birdImage.setVelocityY(100);
-
-    startsGroup.scaleXY(0.3, 0.3);
-    startsGroup2.setVelocityY(150);
-  });
-
-  spaceBar.on('up', () => {
-    console.log('SPACE UP');
-  });
+  return lastStartPos;
 }
 
 function getRandomInt(max) {
@@ -98,33 +96,61 @@ function getRandomInt(max) {
 function update() {
   //console.log('UPDATE')
   //console.log(birdImage.body.position.x, birdImage.body.position.y)
-  if (birdImage.body.position.y > 600) {
+  if(birdImage.body.position.y > 600){
     //birdImage.body.position.y = 300;
     birdImage.setVelocityY(-100)
   }
 
+  if(getLastStarPosition() < config.width){
+    LAST_POS = getLastStarPosition();
+    createStars(this);
+  }
+  
+  //console.log(getLastStarPosition());
+}
 
-  if (cursors.right.isDown) {
-    console.log('SPACE UP   dsd');
-    birdImage.setVelocityX(100);
-    birdImage.setVelocityY(0);
-    //text.x += 5
-  }
-  if (cursors.left.isDown) {
-    birdImage.setVelocityX(-100);
-    birdImage.setVelocityY(0);
-   // text.x -= 5
-  }
-  if (cursors.up.isDown) {
-    birdImage.setVelocityY(-100);
+function collideCallback(box, star){
+  console.log('collideCallback', box, star);
+  star.setVelocityX(60);
+  star.setVelocityY(0);
+}
+
+function processCallback(){
+  console.log('processCallback');
+}
+
+function callbackContext(){
+  console.log('callbackContext');
+}
+
+function handleUserInput(context){
+  var upArrow = context.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+  upArrow.on('down', () => {
+    console.log('SPACE DOWN');
+    birdImage.setVelocityY(-200);
     birdImage.setVelocityX(0);
-   // text.y -= 5
-  }
-  if (cursors.down.isDown) {
-    birdImage.setVelocityY(100);
+  });
+
+  var downArrow = context.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+  downArrow.on('down', () => {
+    console.log('SPACE DOWN');
+    birdImage.setVelocityY(200);
     birdImage.setVelocityX(0);
-    //text.y -= 5
-  }
+  });
 
 
+  var space = context.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+  space.on('down', () => {
+    //1. get Cordinate of Box.
+    //2. Create bomb with same coordinate.
+    //3. Set VelocityX.
+  });
+
+
+
+
+  /*
+  Assignments:
+
+  */
 }
